5bb2b45a75161f5a0ea772b113cf64e55d4bc78f
ahmad <ahmadroyani913@gmail.com>
2020-05-10 13:37:05 +0700

    update galeri

--- content/dash_galeridesa.php
+++ content/dash_galeridesa.php
@@ -19,13 +19,16 @@
                   alt="Card image cap"
                   style="width:350px;height:300px;padding-left:3px;padding-right:5px;padding-top:3px">
                 <div class="card-body" style="background-color:#04B404">
+                <div class="d-flex justify-content-between align-items-center">
                   <p class="card-text text-white"><?= $data['title'] ?></p>
+                  <small  style="color:white ">By <?= $data['updateby'] ?></small>
+                </div>
                   <div class="d-flex justify-content-between align-items-center">
                     <div class="btn-group ">
                       <button type="button" class="btn btn-sm text-white btn-outline-dark">View</button>
                       <button type="button" class="btn btn-sm text-white btn-outline-dark">Edit</button>
                     </div>
-                    <small  style="color:white">9 mins</small>
+                    <small  style="color:white "><?= $data['updatetime'] ?></small>
                   </div>
                 </div>
               </div>
