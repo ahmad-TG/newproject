d4d5b6dbc2b3d08a5f7c4885b75779d3f530d245
ahmad <ahmadroyani913@gmail.com>
2020-06-10 14:12:07 +0700

    perbaikan index

--- index.php
+++ index.php
@@ -4,18 +4,18 @@
   define('INDEX', true);
 ?>
 <!DOCTYPE HTML>
-<html>
+<html lang="en">
    <head>
       <title>Dashboard</title>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" 
+      integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
       <link rel="stylesheet" href="css/dash_style.css" type="text/css">
-      <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" 
-      integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"> -->
-      <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+      <!-- <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css"> -->
       <link rel="stylesheet" href="css/style.css">
       
-      <link rel="stylesheet" href="path/to/bootstrap/css/bootstrap.min.css">
+      <!-- <link rel="stylesheet" href="path/to/bootstrap/css/bootstrap.min.css"> -->
       <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
       <Style>
          .separator{
@@ -51,14 +51,14 @@
               }
               
          }
-         @media screen and (min-width: 480px) and (max-width: 960px){
-            .kabarberita{ background: green; }
+         /* @media screen and (min-width: 480px) and (max-width: 960px){
+            
             .show{
                display:none;
             }
-         }
+         }  */
          @media screen and (min-width: 960px){
-            .kabarberita{ background: blue; }
+            
             .show{
                display:none;
             }
@@ -67,7 +67,7 @@
      
    </head>
    <body class="bgr">
-      <nav class="navbar navbar-expand-lg navbar-light navs bg-success menus" style="z-index:2">
+      <nav class="navbar navbar-expand-lg navbar-light navs bg-success menus" style="z-index:1">
          <ul class="navbar-nav mr-auto">
                <li class="nav-item active">
                   <img src="statics/logo.png"  style="padding-top:20px" width="150" >
@@ -82,28 +82,28 @@
                <div class="col-sm d-flex flex-row-reverse">
                   <ul class="navbar-nav">
                      <li class="nav-item ">
-                        <a class="nav-link h5 text-white active" href="?hal=first_dashboard">HOME</a>
+                        <a class="nav-link h6 text-white active" href="?hal=first_dashboard">HOME</a>
                      </li>
                      <li class="nav-item ">
-                        <a class="nav-link  h5 text-white active" href="?hal=dash_desa">DESA</a>
+                        <a class="nav-link  h6 text-white " href="?hal=dash_desa">DESA</a>
                      </li>
                      <li class="nav-item">
-                        <a class="nav-link h5 text-white"href="?hal=dash_statistik">STATISTIK</a>
+                        <a class="nav-link h6 text-white" href="?hal=dash_statistik">STATISTIK</a>
                      </li>
                      <li class="nav-item">
-                        <a class="nav-link h5 text-white"href="?hal=dash_layanandesa">LAYANAN DESA</a>
+                        <a class="nav-link h6 text-white" href="?hal=dash_layanandesa">LAYANAN DESA</a>
                      </li>
                      <li class="nav-item">
-                        <a class="nav-link h5 text-white"href="?hal=dash_potensidesa">POTENSI DESA</a>
+                        <a class="nav-link h6 text-white" href="?hal=dash_potensidesa">POTENSI DESA</a>
                      </li>
                      <li class="nav-item">
-                        <a class="nav-link h5 text-white"href="?hal=dash_kabardesa">KABAR DESA</a>
+                        <a class="nav-link h6 text-white" href="?hal=dash_kabardesa">KABAR DESA</a>
                      </li>
                      <li class="nav-item">
-                        <a class="nav-link h5 text-white"href="?hal=dash_galeridesa">GALERI DESA</a><a >
+                        <a class="nav-link h6 text-white" href="?hal=dash_galeridesa">GALERI DESA</a><a >
                      </li>
                      <li class="nav-item">
-                        <a class="nav-link h5 text-white" href="login.php">LOGIN</a>
+                        <a class="nav-link h6 text-white" href="login.php">LOGIN</a>
                      </li>
                   </ul>
                </div>
@@ -122,21 +122,23 @@
       </div>
       <footer class="bg-success">
          <!-- <div style="margin-left:10%; margin-right:10%; "> -->
+         
+
          <div >
             <div class="row align-items-center text-white">
                <div class="col-auto">
-               <a class="nav-link hide text-white active" style="margin-left:50px" href="?hal=dash_desa">Desa</a>
-               <a class="h6 show text-white" href="?hal=dash_desa">Desa</a>
+               <!-- <a class="nav-link text-white active" style="margin-left:50px" href="?hal=dash_desa">Desa</a> -->
+               <a class="h6  text-white" href="?hal=dash_desa" style="margin-left:50px">Desa</a>
                </div>
 
                <div class="col-auto">
-               <a class="nav-link hide text-white" href="?hal=dash_statistik">Data</a>
-               <a class="h6 show text-white" href="?hal=dash_statistik">Data</a>
+               <!-- <a class="nav-link text-white" href="?hal=dash_statistik">Data</a> -->
+               <a class="h6  text-white" href="?hal=dash_statistik">Data</a>
                </div>
                
                <div class="col-auto">
-                  <a class="nav-link hide text-white" href="?hal=dash_statistik">Laporan</a>
-                  <a class="h6 show text-white" href="?hal=dash_statistik">Laporan</a>
+                  <!-- <a class="nav-link text-white" href="?hal=dash_statistik">Laporan</a> -->
+                  <a class="h6  text-white" href="?hal=dash_statistik">Laporan</a>
                </div>
 
                <div class="col"></div>
@@ -152,25 +154,26 @@
 
             <div class="row align-items-center text-white">
                <div class="col-auto">
-                  <div class="h6 hide" style="margin-left:50px">©  2019 - Pemerintah Desa Kudukeras </div>
-                  <div class="h6 show" >©  2019 - Pemerintah Desa Kudukeras </div>
+                  <!-- <div class="h6" style="margin-left:50px">©  2019 - Pemerintah Desa Kudukeras </div> -->
+                  <div class="h6 " >©  2019 - Pemerintah Desa Kudukeras </div>
                </div>
                <div class="col"></div>
                <div class="col-auto">
-                  <!-- <div class="h6 hide" style="margin-right:50px">Email : pemdes@kudukeras.desa.id | Cirebon  -->
+                  <!-- <div class="h6 hide" style="margin-right:50px;">Email : pemdes@kudukeras.desa.id | Cirebon  -->
                   <div class="h6 " >Email : pemdes@kudukeras.desa.id | Cirebon
                  
                </div>
             </div>
          </div>
+
       </footer>
 
-      <!-- <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
-      <script src="js/jquery-3.5.0.min.js"></script> -->
+      
+
+      <!-- <script src="bootstrap/js/bootstrap.bundle.min.js"></script> -->
+      <!-- <script src="js/jquery-3.5.0.min.js"></script> -->
       <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-     
+      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
    </body>
 </html>
-
